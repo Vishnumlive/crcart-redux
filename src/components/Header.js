b70930f4fd59755f49b01874165e6c58170ac0bb
Vishnumlive <vishnum.official@gmail.com>
2024-01-03 21:11:28 +0530

    added redux code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,24 @@
 import { Link, NavLink } from "react-router-dom"; 
+import { useSelector } from "react-redux";
 import Logo from "../assets/logo.png"
 import "./Header.css";
 
 export const Header = () => {
+
+  const cartList = useSelector(state=> state.cartState.cartList);
+
   return (
     <header>
       <Link to="/" className="logo">
         <img src={Logo} alt="Shopmate Logo" />
-        <span>Shopping Cart</span>
+        <span>REDUX Cart</span>
       </Link>
       <nav className="navigation">
         <NavLink to="/" className="link" end>Home</NavLink>
         <NavLink to="/cart" className="link">Cart</NavLink>
       </nav>
       <Link to="/cart" className="items">
-        <span>Cart: 2</span>
+        <span>Cart: {cartList.length}</span>
       </Link>
     </header>
   )
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,28 @@
 import "./ProductCard.css";
+import { add, remove } from "../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export const ProductCard = ({product}) => {
-  const {name, price, image} = product;
+  const {id, name, price, image} = product;
+
+  const [isInCart, setIsInCart] = useState(false);
+  const cartList = useSelector(state => state.cartState.cartList);
+
+
+  useEffect(()=>{
+    
+    const productInCart = cartList.find((item) => item.id === id)
+
+    if(productInCart) {
+      setIsInCart(true);
+    }else{
+      setIsInCart(false);
+    }
+
+  },[cartList, id])
+
+  const dispatch = useDispatch();
 
   return (
     <div className="productCard">
@@ -9,7 +30,10 @@ export const ProductCard = ({product}) => {
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button>Add To Cart</button>
+
+        { isInCart ? (<button className="remove" onClick={() => dispatch(remove(product)) }>Remove</button>) : (<button onClick={() => dispatch(add(product)) }>Add To Cart</button>) }
+        
+        
       </div>
     </div>
   )
